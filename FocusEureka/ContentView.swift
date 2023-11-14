4654113cb5a5c1ccc76a2cce67230c0f0a4c66dc
yuzchen7 <yuzchen7@gmail.com>
2023-11-14 01:07:53 -0500

    Initial Commit

---
+++ FocusEureka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusEureka/FocusEurekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusEurekaApp.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusEurekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
