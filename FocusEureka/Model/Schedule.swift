75346734b62f9dbd94443eafea8bbf448e146804
yuzchen7 <yuzchen7@gmail.com>
2023-11-16 15:11:46 -0500

    Refactor: Port Scheule model from previous crash project

---
+++ FocusEureka/Model/Schedule.swift
@@ -0,0 +1,35 @@
+//
+//  Schedule.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 11/16/23.
+//
+
+import Foundation
+
+struct Schedule: Identifiable, Codable {
+    var id: Int
+    var scheduleDay: [ScheduleDay];
+    
+    enum CodingKeys:  String, CodingKey {
+        case id = "user_id",
+             scheduleDay = "schedules"
+    }
+    
+}
+
+struct ScheduleDay: Identifiable, Codable {
+    var id: Int = UUID().hashValue
+    var day: String
+    var isAvaliable: Bool
+    
+    enum CodingKeys:  String, CodingKey {
+        case day = "day",
+             isAvaliable = "isAvaliable"
+    }
+    
+    mutating func setIsAvaliable(isAvaliable: Bool) {
+        self.isAvaliable = isAvaliable
+    }
+    
+}
