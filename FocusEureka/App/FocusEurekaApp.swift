a84eb09c3d7200271dac4460535291f292c0104b
yuzchen7 <yuzchen7@gmail.com>
2023-11-16 15:30:09 -0500

    Refactor: Port auto login feature from previous crash project

--- FocusEureka/App/FocusEurekaApp.swift
+++ FocusEureka/App/FocusEurekaApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct FocusEurekaApp: App {
+    @StateObject var loginViewModel: LoginViewModel = LoginViewModel()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(loginViewModel)
+                .onAppear {
+                    print("AUTO LOGIN:\n\tIts possible to log the error retrieve/save message for the auto signIn feature\n\tignore the follwing message  ╮（￣▽￣）╭ ->")
+                    if
+                        let savedUsername = loginViewModel.retrieveItem(forkey: "username"),
+                        let password = loginViewModel.retrieveItem(forkey: savedUsername)
+                    {
+                        loginViewModel.signIn(username:savedUsername , password: password)
+                    }
+                }
         }
     }
 }
