031f4ca21794ebecd0676a008f189e4f21ecc369
yuzchen7 <yuzchen7@gmail.com>
2023-11-16 15:24:36 -0500

    feat: display login page for user

--- FocusEureka/Views/Root/ContentView.swift
+++ FocusEureka/Views/Root/ContentView.swift
@@ -8,14 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var loginViewModel: LoginViewModel;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Group {
+            if (self.loginViewModel.sessionUser != nil) {
+                AccountView()
+            } else {
+                LoginView()
+            }
         }
-        .padding()
     }
 }
 
