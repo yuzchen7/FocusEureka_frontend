3b2e89dd219c51e01ec7b2dbcb3adf51a551a72d
yuzchen7 <yuzchen7@gmail.com>
2023-11-17 11:36:13 -0500

    feat: impl tap to change is avaliable in schedule

--- FocusEureka/Views/Scheule/View/ScheuleView.swift
+++ FocusEureka/Views/Scheule/View/ScheuleView.swift
@@ -13,11 +13,18 @@ struct ScheuleView: View {
     @EnvironmentObject var loginViewModel: LoginViewModel
     
     var body: some View {
-        if let schedules = scheduleViewModel.schedule {
+        if let schedules = scheduleViewModel.schedule, scheduleViewModel.isUpdate == false {
             VStack {
                 // single scheule view
-                ForEach(schedules.scheduleDay) {element in
-                    SingleScheuleView(day: element.day, isAvailable: element.isAvaliable)
+                ForEach(schedules.scheduleDay.indices, id: \.self) {index in
+                    Button(action: {
+                        print("Click able")
+                        scheduleViewModel.updateScheduleData(userId: loginViewModel.currentUser!.id, at: index, isAvaliable: !(schedules.scheduleDay[index].isAvaliable))
+                        print(schedules.scheduleDay)
+                    }, label: {
+                        let element = schedules.scheduleDay[index]
+                        SingleScheuleView(index: index, day: element.day, isAvailable: element.isAvaliable)
+                    })
                 }
             }
         } else {
@@ -27,4 +34,8 @@ struct ScheuleView: View {
                 }
         }
     }
+}
+
+#Preview {
+    ScheuleView(scheduleViewModel: ScheduleViewModel())
 }
