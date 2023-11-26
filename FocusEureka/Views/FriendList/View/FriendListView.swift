7bd66fb1bc6e2c85a70b80d6d4ba37084bf0dc93
yuzchen7 <yuzchen7@gmail.com>
2023-11-26 17:30:38 -0500

    init: 1st ver of firend list layout

--- FocusEureka/Views/FriendList/View/FriendListView.swift
+++ FocusEureka/Views/FriendList/View/FriendListView.swift
@@ -8,8 +8,22 @@
 import SwiftUI
 
 struct FriendListView: View {
+    @ObservedObject var friendListModel: FriendListModel = FriendListModel()
+    @EnvironmentObject var loginViewModel: LoginViewModel
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if true {
+            VStack {
+                ForEach(self.friendListModel.testingData.indices, id: \.self) {index in
+                    let currentUser = friendListModel.testingData[index]
+                    UserSingleCardView(initials: currentUser.initials, fullname: currentUser.fullName)
+                }
+            }
+        } else {
+            Text("Loading...")
+                .onAppear {
+                    friendListModel.getFriendList(id: loginViewModel.currentUser!.id)
+                }
+        }
     }
 }
 
