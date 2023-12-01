9f73022845828ac40c5fe0f04f91cfa37295cba5
yuzchen7 <yuzchen7@gmail.com>
2023-12-01 01:03:33 -0500

    init: requestor list view

--- FocusEureka/Views/FriendList/View/RequestFriendView.swift
+++ FocusEureka/Views/FriendList/View/RequestFriendView.swift
@@ -8,8 +8,47 @@
 import SwiftUI
 
 struct RequestFriendView: View {
+    @ObservedObject var requestFriendModel: RequestFriendModel = RequestFriendModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Search Friends").font(.title2).foregroundStyle(.pink)
+            
+            if let resultfriendList = RequestFriendModel.testingData, !resultfriendList.isEmpty {
+                List {
+                    ForEach(resultfriendList.indices, id: \.self) {index in
+                        let currentUser = resultfriendList[index]
+                        
+                        Button(action: {
+                            
+                        }, label: {
+                            ZStack {
+                                UserSingleCardView(initials: currentUser.initials, fullname: currentUser.fullName)
+                                    .frame(height: 40)
+                                
+                                HStack {
+                                    Spacer()
+                                    Button(action: {
+                                        
+                                    }, label: {
+                                        Image(systemName: "plus.app")
+                                            .resizable()
+                                            .foregroundStyle(Color.pink.opacity(0.8))
+                                            .frame(width: 30, height: 30)
+                                            .padding(.horizontal)
+                                    })
+                                }
+                            }
+                        })
+                    }
+                }
+                .listStyle(PlainListStyle())
+                
+            } else {
+                
+            }
+        }
+        .padding(.top)
     }
 }
 
