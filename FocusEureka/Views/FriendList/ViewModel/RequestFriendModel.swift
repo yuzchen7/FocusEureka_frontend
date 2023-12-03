6ee0dbdeb6700c7526544815ff64b5a285313427
yuzchen7 <yuzchen7@gmail.com>
2023-12-03 00:50:00 -0500

    feat: impl reject friend request by fetch

--- FocusEureka/Views/FriendList/ViewModel/RequestFriendModel.swift
+++ FocusEureka/Views/FriendList/ViewModel/RequestFriendModel.swift
@@ -54,13 +54,34 @@ class RequestFriendModel: ObservableObject {
                     self.isUpdate = !self.isUpdate
                 }
             } catch Swiftxios.FetchError.invalidURL {
-                print("function requestFriendFetch from class Swiftxios has URL error (╯’ – ‘)╯︵")
+                print("function acceptFriendRequestFetch from class Swiftxios has URL error (╯’ – ‘)╯︵")
             } catch Swiftxios.FetchError.invalidResponse {
-                print("function requestFriendFetch from class Swiftxios has HttpResponse error (╯’ – ‘)╯︵")
+                print("function acceptFriendRequestFetch from class Swiftxios has HttpResponse error (╯’ – ‘)╯︵")
             } catch Swiftxios.FetchError.invalidData {
-                print("function requestFriendFetch from class Swiftxios has response Data error (╯’ – ‘)╯︵")
+                print("function acceptFriendRequestFetch from class Swiftxios has response Data error (╯’ – ‘)╯︵")
             } catch Swiftxios.FetchError.invalidObjectConvert {
-                print("function requestFriendFetch from class Swiftxios has Converting Data error (╯’ – ‘)╯︵")
+                print("function acceptFriendRequestFetch from class Swiftxios has Converting Data error (╯’ – ‘)╯︵")
+            } catch {
+                print("unknow error -> unexpected \(error.localizedDescription) (╯’ – ‘)╯︵")
+            }
+        }
+    }
+    
+    func declineFriendRequestFetch(id: Int, targetID: Int) {
+        Task {
+            do {
+                print("declineFriendRequestFetch run -> userID : \(id), targetID : \(targetID)")
+                let urlString: String = "http://localhost:8080/api/friend_request/rejectRequest?currentUser=\(id)&sender=\(targetID)"
+                var _: Int? = try await swiftxios.delete(urlString, ["application/json" : "Content-Type"])
+                self.isUpdate = true
+            } catch Swiftxios.FetchError.invalidURL {
+                print("function declineFriendRequestFetch from class Swiftxios has URL error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidResponse {
+                print("function declineFriendRequestFetch from class Swiftxios has HttpResponse error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidData {
+                print("function declineFriendRequestFetch from class Swiftxios has response Data error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidObjectConvert {
+                print("function declineFriendRequestFetch from class Swiftxios has Converting Data error (╯’ – ‘)╯︵")
             } catch {
                 print("unknow error -> unexpected \(error.localizedDescription) (╯’ – ‘)╯︵")
             }
