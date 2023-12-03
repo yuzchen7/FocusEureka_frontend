bba869d5a839891b36785eb8b8df292c8ab4dd5a
yuzchen7 <yuzchen7@gmail.com>
2023-12-02 20:44:10 -0500

    feat: impl requestFriendFetch api call

--- FocusEureka/Views/FriendList/ViewModel/RequestFriendModel.swift
+++ FocusEureka/Views/FriendList/ViewModel/RequestFriendModel.swift
@@ -11,6 +11,25 @@ import Foundation
 class RequestFriendModel: ObservableObject {
     @Published var requestFriendList: [User]? = nil
 
+    func requestFriendFetch(id: Int) {
+        Task {
+            do {
+                print("requestFriendFetch run -> user ID : \(id)")
+                let urlString: String = "http://localhost:8080/api/friend_request/receiving?targetid=\(id)"
+                self.requestFriendList = try await swiftxios.get(urlString, ["application/json" : "Content-Type"])
+            } catch Swiftxios.FetchError.invalidURL {
+                print("function requestFriendFetch from class Swiftxios has URL error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidResponse {
+                print("function requestFriendFetch from class Swiftxios has HttpResponse error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidData {
+                print("function requestFriendFetch from class Swiftxios has response Data error (╯’ – ‘)╯︵")
+            } catch Swiftxios.FetchError.invalidObjectConvert {
+                print("function requestFriendFetch from class Swiftxios has Converting Data error (╯’ – ‘)╯︵")
+            } catch {
+                print("unknow error -> unexpected \(error.localizedDescription) (╯’ – ‘)╯︵")
+            }
+        }
+    }
 }
 
 extension RequestFriendModel {
