7f02c61dd37c5a3c64d22644dbba39999f70e47c
yuzchen7 <yuzchen7@gmail.com>
2023-12-05 19:48:19 -0500

    fix: change build setting

---
+++ FocusEureka/Views/OtherUser/View/OtherUserView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherUserView.swift
+//  FocusEureka
+//
+//  Created by yuz_chen on 12/5/23.
+//
+
+import SwiftUI
+
+struct OtherUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherUserView()
+}
