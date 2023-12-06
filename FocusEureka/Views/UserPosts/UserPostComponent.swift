f04d0587b6fc47887af62a7281970a962b77de81
kaifeng yu <kaifeng.yu87@bcmail.cuny.edu>
2023-12-05 21:18:17 -0500

    add UserPostComponent file for user personal view

---
+++ FocusEureka/Views/UserPosts/UserPostComponent.swift
@@ -0,0 +1,57 @@
+//
+//  UserPostsView.swift
+//  FocusEureka
+//
+//  Created by kai on 12/5/23.
+//
+
+import SwiftUI
+
+struct UserPostComponent: View {
+    var pinterestView: [GridItem] = [
+        .init(.flexible())
+    ]
+    var postLColumn:[Posts]
+    var postRColumn:[Posts]
+    @EnvironmentObject var postVM : PostsViewModel
+    var body: some View {
+        ScrollView(showsIndicators: false){
+            HStack(alignment: .top, spacing: 1){
+                LazyVGrid(columns: pinterestView) {
+                    ForEach(postLColumn){ post in
+                        NavigationLink(
+                            value: post
+                        ){
+                            CardComponent(imageURL: post.image_set.urls[0], title: post.title, Likes: post.post_likes?.count ?? 0, posterName: post.owner.username,
+                                          postId:post.id,
+                                          userId: 1
+                            )
+                        }
+                    }
+                }
+                LazyVGrid(columns: pinterestView) {
+                    ForEach(postRColumn){ post in
+                        NavigationLink(
+                            value: post
+                        ){
+                            CardComponent(imageURL: post.image_set.urls[0], title: post.title, Likes: post.post_likes?.count ?? 0, posterName: post.owner.username,
+                                          postId:post.id,
+                                          userId: 1)
+                        }
+                    }
+                }
+            }
+        }
+        .background(.gray.opacity(0.2))
+//        .navigationTitle("Interesting Spot")
+//        .navigationBarTitleDisplayMode(.inline)
+        .navigationDestination(for: Posts.self, destination: { detailPost in
+            PostDetailView(detailedPost: detailPost)
+                .environmentObject(postVM)
+        })
+    }
+}
+
+#Preview {
+    UserPostComponent(postLColumn: [], postRColumn: [])
+}
